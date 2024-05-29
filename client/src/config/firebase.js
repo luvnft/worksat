a7953d2171c1cc3678c87b11876dd84bf1529e8b
luvnft <140175751+luvnft@users.noreply.github.com>
2024-05-29 03:19:30 -0400

    Update firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5ccRszH3U24VQ0tsexMMXGJRrPbi-gzU",
-  authDomain: "worksat-e6abe.firebaseapp.com",
-  projectId: "worksat-e6abe",
-  storageBucket: "worksat-e6abe.appspot.com",
-  messagingSenderId: "827889633714",
-  appId: "1:827889633714:web:5e5fbd0b1e2e9e7d813cab",
-  measurementId: "G-8VE9BDD2YS",
+  apiKey: "AIzaSyB1MtioCmu5F4kr25uaZayOY639-xVBif8",
+  authDomain: "worksat-444e6.firebaseapp.com",
+  projectId: "worksat-444e6",
+  storageBucket: "worksat-444e6.appspot.com",
+  messagingSenderId: "457195495356",
+  appId: "1:457195495356:web:6af8de2e09183a8a3ad5b9",
+  measurementId: "G-T8MQJFXNDL",
 };
 
 const app = initializeApp(firebaseConfig);
